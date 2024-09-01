a2c26844c7b99d83d9205811b15f32e347b1a906
Leela Sai Prasanth <59244101+leelasaiprasanth@users.noreply.github.com>
2024-09-01 22:09:10 +0530

    Fix Avatar not Loading

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+// if the google account athentication and display does not work
+// then use this
+// =================================
+// const nextConfig = {
+//   async headers() {
+//     return [
+//       {
+//         source: '/:path*',
+//         headers: [
+//           {key: 'referrer-policy', value: 'no-referrer'},
+//         ]
+//       }
+//     ]
+//   }
+// }
+
 export default nextConfig;

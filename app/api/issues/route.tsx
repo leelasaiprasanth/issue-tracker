40fd89a72813c57cb2e4fccbb31a4e5873711023
Leela Sai Prasanth <59244101+leelasaiprasanth@users.noreply.github.com>
2024-09-01 14:49:39 +0530

    Build an API for Updating issue

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -1,12 +1,11 @@
 import { NextResponse } from "next/server";
 import { z } from "zod";
 import prisma from "@/prisma/client";
-import { createIssueSchema } from "./validationSchemas";
-
+import { issueSchema } from "./validationSchemas";
 
 export async function POST(request: NextResponse) {
   const body = await request.json();
-  const validation = createIssueSchema.safeParse(body);
+  const validation = issueSchema.safeParse(body);
 
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { createIssueSchema } from "@/app/api/issues/validationSchemas";
+import { issueSchema } from "@/app/api/issues/validationSchemas";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -17,7 +17,7 @@ const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false,
 });
 
-type IssueFormData = z.infer<typeof createIssueSchema>;
+type IssueFormData = z.infer<typeof issueSchema>;
 
 const IssueForm = ({ issue }: { issue?: Issue }) => {
   const router = useRouter();
@@ -27,7 +27,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
     handleSubmit,
     formState: { errors },
   } = useForm<IssueFormData>({
-    resolver: zodResolver(createIssueSchema),
+    resolver: zodResolver(issueSchema),
   });
 
   const [error, setError] = useState("");
